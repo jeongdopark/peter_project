028797a1ac854d640238f6067ea3e1b741d0d82a
jeongdopark <jdpark0816@gmail.com>
2022-11-30 01:07:19 +0900

    login form

--- frontend/gallery/gallery.js
+++ frontend/gallery/gallery.js
@@ -1,10 +1,15 @@
 
-
 const carousel_container = document.querySelector('.carousel_container');
 const left_arrow = document.querySelector('.left_arrow');
 const right_arrow = document.querySelector('.right_arrow');
+const main_gallery_container = document.querySelector('.main_gallery_container');
+const items = document.querySelectorAll('.main_gallery_img');
+const main_gallery = document.querySelector('.main_gallery');
 let index = 0;
 let current_X = 0;
+
+
+
 right_arrow.addEventListener('click', () => {
     if(index > -2){
         index -= 1;
@@ -30,3 +35,53 @@ left_arrow.addEventListener('click', () => {
     }
     console.log(index);
 })
+
+// 무한 스크롤
+
+let img_number = 11;
+
+const addNewContent = () => {
+    let division = document.createElement('div');
+    division.classList.add('main_gallery_division');
+
+    const make_element = () => {
+        Array.from({length : 5}, () => {
+            const image = new Image();
+            let element_wrapper = document.createElement('span');
+            element_wrapper.classList.add('main_gallery_img');
+            image.src = `../../image/gallery_img/IMG_000${img_number}.JPG`;
+            element_wrapper.appendChild(image);
+            division.appendChild(element_wrapper);
+            img_number += 1
+        })
+        
+        main_gallery_container.appendChild(division);
+        main_gallery.appendChild(main_gallery_container);
+    }
+    make_element()
+}   
+
+
+
+const ioCallback = (entries, io) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        io.unobserve(entry.target);
+        setTimeout(() => {
+        console.log('hello');
+          addNewContent();
+          observeLastItem(io, document.querySelectorAll('.items.main_gallery_img'));
+        }, 2000);
+      }
+    });
+  };
+  
+  const observeLastItem = (io, items) => {
+    const item = document.querySelectorAll('.main_gallery_img');
+    const lastItem = item[item.length - 1];
+    io.observe(lastItem);
+    console.log(item);
+  };
+  
+  const io = new IntersectionObserver(ioCallback, { threshold: 0.7 });
+  observeLastItem(io, main_gallery);
--- frontend/main/main.js
+++ frontend/main/main.js
@@ -1,6 +1,37 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 let toggle = document.querySelector('.toggle');
 let menu = document.querySelector('.menu');
-
+let menu_login = document.querySelector('.login');
+let login_modal_container = document.querySelector('.login_modal_container');
+let x_button = document.querySelector('.x_button');
 toggle.addEventListener('click', () => {
     menu.classList.toggle('active');
 })
+
+menu_login.addEventListener('click', () => {
+    login_modal_container.classList.add('seen');
+    login_modal_container.classList.remove('remove');
+})
+
+x_button.addEventListener('click', () => {
+    login_modal_container.classList.remove('seen');
+    login_modal_container.classList.add('remove');
+})
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAqdMYDEQ9wWLJ0fCPfKA2x_Isl-qBD9cg",
+  authDomain: "peter-login.firebaseapp.com",
+  projectId: "peter-login",
+  storageBucket: "peter-login.appspot.com",
+  messagingSenderId: "951634914028",
+  appId: "1:951634914028:web:32dbf6d872043379f293c9",
+  measurementId: "G-LFXJ7YQFD4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
